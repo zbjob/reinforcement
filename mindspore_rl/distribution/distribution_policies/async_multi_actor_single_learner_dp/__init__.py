5211d4e62cecf737c5505e9e598fa0a0fe768388
Huanzhou Zhu <hz04@ic.ac.uk>
2023-02-02 10:18:50 +0000

    auto distribution

---
+++ mindspore_rl/distribution/distribution_policies/async_multi_actor_single_learner_dp/__init__.py
@@ -0,0 +1,6 @@
+"""
+Asy Multi actor environment single learner policy
+"""
+
+from .async_multi_actor_single_learner_dp import AsyncMultiActorSingleLearnerDP
+__all__ = ["AsyncMultiActorSingleLearnerDP"]
---
+++ mindspore_rl/distribution/distribution_policies/multi_actor_single_learner_dp/__init__.py
@@ -0,0 +1,6 @@
+"""
+Multi actor environment single learner policy
+"""
+
+from .multi_actor_single_learner_dp import MultiActorEnvSingleLearnerDP
+__all__ = ["MultiActorEnvSingleLearnerDP"]
